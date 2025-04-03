6bc57c0093b26a4591d5f2c62dd26b4d0d23f3ad
GitHub Action <action@github.com>
2025-04-03 12:22:43 +0000

    Add new outage posts

--- _data/outages.js
+++ _data/outages.js
@@ -2,6 +2,6 @@ module.exports = [
   {
     "date": "2025-04-03",
     "title": "Fallback Outage",
-    "content": "No outages detected from Outage.Report"
+    "content": "No outages detected from PowerOutage.us"
   }
 ];
